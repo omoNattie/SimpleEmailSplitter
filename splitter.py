b0a2a0b2e3bae1669ab24fbd676fce6b653fa809
omoNattie <98173891+omoNattie@users.noreply.github.com>
2022-05-28 00:28:07 +0300

    removed extension feature

--- splitter.py
+++ splitter.py
@@ -10,10 +10,7 @@ elif "@" not in email:
     print("You need a domain.")  # if "@" is missing
 else:
     name = email[:email.index("@")]  # get everything before "@"
-    domain = email[email.index("@"):]  # get everything after "@"
-    domains = domain.split(".")[:-1]  # split everything before "."
-    domains = ".".join(domains)  # put it into a string and not a list
-    extension = email[email.index("."):]  # get everything after "."
+    domain = email[email.index("@")+1:]  # get everything after "@"
 
-    final = "Name: {}\nDomain: {}\nExtension: {}".format(name, domains, extension)
+    final = "Name: {}\nDomain: {}".format(name, domain)
     print(final)
